affd45af6d521c1c631b3b2550a8678a1b7e418e
Carl Tobias <saibotrics@icloud.com>
2022-10-16 15:58:45 +0400

    pir2

--- variables.py
+++ variables.py
@@ -29,7 +29,7 @@ print("The area of your square is:", area, ".")
 from math import pi
 
 r = float(input("Input the radius of the circle: "))
-print("The area of the circle with radius " + str(r) + " is: " + str(pi**2))
+print("The area of the circle with radius " + str(r) + " is: " + str(pi*r**2))
 
 #now for rectangle
 
